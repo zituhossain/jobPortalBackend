5dabece0b2b413239caf8b1cb1f73427d1220558
zituhossain5 <zituhossain@gmail.com>
2024-08-29 12:49:34 +0600

    updated

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "https://job-portal-two-black.vercel.app",
+  origin: "*",
   credentials: true,
   optionsSuccessStatus: 200,
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
+app.options("", cors(corsOptions));
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
