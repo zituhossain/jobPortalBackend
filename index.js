01a9698ab8505ef643e1224f30638a4d575ea816
zituhossain5 <zituhossain@gmail.com>
2024-08-31 14:22:42 +0600

    update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://jobportalfrontend-r7o2.onrender.com",
   credentials: true,
   optionsSuccessStatus: 200,
 };
